4347ff4bde6734289eee4e954d6d4c124fc6e5e1
kuropenguin <kuro1994.t@gmail.com>
2023-01-22 15:44:28 +0900

    fix init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+if [ -d ~/.oh-my-zsh ]; then :; else
+    echo "install oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 cp .zshrc $HOME/.zshrc
 
 source $HOME/.zshrc
+
+echo "init settings done"
